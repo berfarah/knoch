cbd7d44d657ace49b248abf4320f02da0a2b9206
Bernardo Farah <ber@bernardo.me>
2017-08-28 16:56:58 -0700

    Implement version flag

---
+++ internal/version.go
@@ -0,0 +1,20 @@
+package internal
+
+import (
+	"github.com/berfarah/knoch/internal/command"
+	"github.com/berfarah/knoch/internal/utils"
+)
+
+// VERSION of knoch
+const VERSION = "0.1.0"
+
+func init() {
+	Runner.Register(&command.Command{
+		Run:  runVersion,
+		Name: "version",
+	})
+}
+
+func runVersion(c *command.Command, r *command.Runtime) {
+	utils.Println("knoch version " + VERSION)
+}
